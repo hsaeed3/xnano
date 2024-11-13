184416bfa956d1ff18f6a497812e80285b091eb2
hsaeed <hsaeed@csod.com>
2024-11-13 01:29:11 -0800

    🏗️ PATCH: update imports for circular import fix

--- xnano/types/completions/responses.py
+++ xnano/types/completions/responses.py
@@ -1,13 +1,14 @@
+# response
 from pydantic import BaseModel
 from typing import Union, Type, List
 from ._openai import ChatCompletion
-from ..models.mixin import BaseModelMixin as BMM
+from ..models.mixin import BaseModelMixin
 
 # response
 Response = Union[
     # standard completion
     ChatCompletion, List[ChatCompletion],
-    BMM, List[BMM],
+    BaseModelMixin, List[BaseModelMixin],
     # all structured output formats
     Type[BaseModel], List[Type[BaseModel]],
     str, list[str], int , float, bool, list[int], list[float], list[bool], list
