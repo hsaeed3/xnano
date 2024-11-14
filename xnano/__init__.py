87e6fcab2886fefb0c58b03ebf34a2aa9c96381b
hsaeed <hsaeed@csod.com>
2024-11-13 19:20:55 -0800

    🏗️ PATCH: build in message & pydantic utils

--- xnano/__init__.py
+++ xnano/__init__.py
@@ -28,6 +28,11 @@ __all__ = [
     "console",
     "patch",
     "unpatch",
+    "messages",
+    "format_messages",
+    "swap_system_prompt",
+    "add_message",
+    "repair_messages",
 ]
 
 # imports
@@ -64,3 +69,13 @@ from .data import (
 from .multimodal import generate_image, generate_audio, generate_transcription
 
 from .tools import TOOLS
+
+from .utils import (
+    messages,
+    add_message,
+    format_messages,
+    swap_system_prompt,
+    repair_messages,
+    convert_yaml_to_pydantic,
+    convert_to_openai_tool,
+)
