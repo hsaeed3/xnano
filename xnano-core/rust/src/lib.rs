3b45b3f21496e04430654f55f304873b69e8dc9f
hsaeed3 <hammadaidev@gmail.com>
2026-07-01 21:41:28 -0700

    feat(refactor): migrate to ``xnano-core`` subpackage for rust bindings in favor of compartamentalized access to primitives and modules

--- xnano-core/rust/src/lib.rs
+++ xnano-core/rust/src/lib.rs
@@ -5,6 +5,6 @@ use pyo3::prelude::*;
 // Native rust bindings for ``ratatui`` & ``tachyonfx`` primitives represented as
 // ``xnano_core._xnano_core``.
 #[pymodule]
-fn _core(module: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _xnano_core(module: &Bound<'_, PyModule>) -> PyResult<()> {
     bindings::register(module)
 }
