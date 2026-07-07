3a139895728389d2e9ee31f9457785e539cde8f6
hsaeed3 <hammadaidev@gmail.com>
2026-07-07 05:32:52 -0700

    docs(xnano): add package-level doctest import examples

--- xnano/__init__.py
+++ xnano/__init__.py
@@ -1,4 +1,8 @@
-"""xnano"""
+"""xnano
+
+>>> import xnano.beta as x
+>>> from xnano.beta import Grid, Field, Terminal
+"""
 
 from xnano.beta.core.version import VERSION, _ensure_xnano_core_version
 
