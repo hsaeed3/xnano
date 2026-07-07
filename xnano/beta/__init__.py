13e63007694e26a5a699d08872c69f59fdcd2c96
hsaeed3 <hammadaidev@gmail.com>
2026-07-06 20:01:33 -0700

    chore: stage ``xnano/beta`` as core module for all ``0.9x`` releases until ``1.0.0``

---
+++ xnano/beta/__init__.py
@@ -0,0 +1,9 @@
+"""xnano.beta
+
+Beta implementation of the complete ``xnano`` framework used
+as a staging ground until the first stable ``1.0.0``
+release.
+
+All exports and components currently within ``xnano`` are all 
+within this submodule.
+"""
