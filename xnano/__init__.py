ce661fa6ba849923839aec4a93f30c34fac2bc69
hsaeed3 <hammadaidev@gmail.com>
2026-07-07 18:04:02 -0700

    chore(versioning): update versioning and import structure for top level `xnano` impots to allow for lazy loaded access

--- xnano/__init__.py
+++ xnano/__init__.py
@@ -4,9 +4,4 @@
 >>> from xnano.beta import Grid, Field, Terminal
 """
 
-from xnano.beta.core.version import VERSION, _ensure_xnano_core_version
-
-_ensure_xnano_core_version()
-del _ensure_xnano_core_version
-
-__version__ = VERSION
+__version__ = "1.0.0b1"
--- xnano/beta/core/__init__.py
+++ xnano/beta/core/__init__.py
@@ -1,1 +1,8 @@
 """xnano.beta.core"""
+
+# version constraint check happens on first import
+# of xnano.beta.core
+from xnano.beta.core.version import _ensure_xnano_core_version
+
+_ensure_xnano_core_version()
+del _ensure_xnano_core_version
