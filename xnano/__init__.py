5d43bdfe47eb76a0fdf185575f33c1b20db8916c
hsaeed3 <hammadaidev@gmail.com>
2026-07-29 00:06:07 -0700

    chore(release): bump `xnano` version to `v1.2.0`

--- xnano/__init__.py
+++ xnano/__init__.py
@@ -16,7 +16,7 @@ try:
 except (
     importlib.metadata.PackageNotFoundError
 ):  # pragma: no cover - editable / source trees
-    __version__ = "1.1.8"
+    __version__ = "1.2.0"
 
 if TYPE_CHECKING:
     from xnano import cli, components, core, events, hooks, requests
